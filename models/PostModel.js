76ade2583cae485904118263779c85ce977bf270
Javed Ansari <javed.ansari4122@gmail.com>
2021-05-23 10:16:45 +0530

    Fixed some Image Loading Bugs

--- models/PostModel.js
+++ models/PostModel.js
@@ -23,7 +23,7 @@ const PostSchema = new mongoose.Schema({
     },
     url: {
         type: String,
-        default: 'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'
+        default: process.env.DEFAULT_POST_IMAGE
     }
 }, { timestamps: true })
 
--- models/UserModel.js
+++ models/UserModel.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
     },
     url: {
         type: String,
-        default: 'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'
+        default: process.env.DEFAULT_PROFILE_IMAGE
     },
     following: [{
         type: mongoose.Schema.Types.ObjectId,
