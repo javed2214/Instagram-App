2ed16d1a59db76e54340c0dcbb6ce4f8808a812f
Javed Ansari <javed.ansari4122@gmail.com>
2021-05-25 03:27:48 +0530

    Fixed some Bugs in app.js

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use('/private', require('./routes/private'))
 
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'))
+    const path = require('path');
+  	app.get('*', (req, res) => {
+    	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  	});
 }
 
 const PORT = process.env.PORT || 7000
